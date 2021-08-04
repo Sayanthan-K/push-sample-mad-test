e654d929f9dd9b30f364d46393aaf9cb77076db1
Sayanthan <kathirsayanthan@gmail.com>
2021-08-04 09:59:28 +0530

    this user11 and commited

---
+++ app/src/main/java/com/example/lab1first/someexample.java
@@ -0,0 +1,14 @@
+package com.example.lab1first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class someexample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_someexample);
+    }
+}
